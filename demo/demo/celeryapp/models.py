10163ecfedc70b0cbd71af719a0fbedb9fcd8d19
Mike Lopez <dev@scidentify.info>
2013-08-01 09:48:00 -0400

    quick model and test to get the app ready for celery

--- demo/demo/celeryapp/models.py
+++ demo/demo/celeryapp/models.py
@@ -1,3 +1,25 @@
+from datetime import datetime, timedelta
 from django.db import models
 
 # Create your models here.
+class Dog(models.Model):
+    """ Dog class to keep track of all the dogs and their poop. """
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    bday = models.DateTimeField()
+
+class Stool(models.Model):
+    """ Keep track of the poops that the dogs take """
+    TYPES_OF_POOP = (
+            ('good', 'good'),
+            ('moisty', 'moisty'),
+            ('pretty solid', 'pretty solid'),
+            ('muddy', 'muddy'),
+            ('diarrhea for sure', 'diarrhea for sure'),
+            ('bloody', 'bloody'),
+            ('parasites', 'parasites'),
+            ('an earplug', 'an earplug'),
+    )
+    type_of_poop = models.CharField(max_length=30, choices=TYPES_OF_POOP)
+    date_taken = models.DateTimeField(default=datetime.now())
+    dog = models.ForeignKey('Dog')
--- demo/demo/celeryapp/tests.py
+++ demo/demo/celeryapp/tests.py
@@ -1,16 +1,69 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+Test the dog and stool models. 
 """
 
 from django.test import TestCase
-
+from models import *
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def __add_dogs(self):
+        """ Internal helper to add dogs """
+        dopey = Dog.objects.create(name="Dopey", age=6, bday=datetime(2007, 04, 16, 0,0,0))
+        jiggles = Dog.objects.create(name="Jiggles", age=1, bday=datetime(2012, 12, 28, 0,0,0))
+
+    def __delete_dogs(self):
+        """ Internal helper to delete teh dogs after a test """
+        for i in Dog.objects.all():
+            i.delete()
+
+    def test_dog(self):
+        """ Test adding the dogs """
+        self.__add_dogs()
+        self.__delete_dogs()
+    
+    def test_stools(self):
+        """ Test adding the stool records for the dogs """
+        self.__add_dogs()
+        dopey = Dog.objects.get(name='Dopey')
+        jiggles = Dog.objects.get(name='Jiggles')
+        self.assertTrue(dopey)
+        self.assertTrue(jiggles)
+        # add the stools
+        yesterday = (datetime.now() - timedelta(days=1))
+        today = datetime.now()
+
+        dopey_yesterday = {'type_of_poop': 'moisty', \
+                        'date_taken': yesterday, 'dog': dopey}
+        dopey_today = {'type_of_poop': 'good', \
+                        'date_taken': yesterday, 'dog': dopey}
+        yesterdays_poop = Stool(**dopey_yesterday)
+        yesterdays_poop.save()
+        todays_poop = Stool(**dopey_today)
+        todays_poop.save()
+        yesterday_result = "Yesterday, %s took a poop and it was %s" % (\
+                        dopey.name, yesterdays_poop.type_of_poop)
+        today_result = "Today, %s took a poop and it was %s" % (\
+                       dopey.name, todays_poop.type_of_poop)
+        self.assertEquals(yesterday_result, \
+                    "Yesterday, Dopey took a poop and it was moisty")
+        self.assertEquals(today_result, \
+                    "Today, Dopey took a poop and it was good")
+
+
+        jiggles_yesterday = {'type_of_poop': 'earplug', \
+                        'date_taken': yesterday, 'dog': jiggles}
+        jiggles_today = {'type_of_poop': 'good', \
+                        'date_taken': yesterday, 'dog': jiggles}
+        yesterdays_poop = Stool(**jiggles_yesterday)
+        yesterdays_poop.save()
+        todays_poop = Stool(**jiggles_today)
+        todays_poop.save()
+        yesterday_result = "Yesterday, %s took a poop and it was %s" % (\
+                        jiggles.name, yesterdays_poop.type_of_poop)
+        today_result = "Today, %s took a poop and it was %s" % (\
+                       jiggles.name,todays_poop.type_of_poop)
+        self.assertEquals(yesterday_result, \
+                    "Yesterday, Jiggles took a poop and it was earplug")
+        self.assertEquals(today_result, \
+                    "Today, Jiggles took a poop and it was good")
+
